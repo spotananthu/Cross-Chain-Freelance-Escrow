fa6e9decbe1741dc47401a86b94c0a31d0ebd9e6
Anantha Krishnan <ananthutemp@gmail.com>
2026-01-31 23:43:37 +0530

    [+] Add frontend configuration files

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  images: {
+    domains: ['avatar.vercel.sh'],
+  },
+};
+
+module.exports = nextConfig;
